56fc05c2d9d7e34ddf64e4844c5f4eb26f53eb65
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2023-05-06 16:51:53 +0700

    [UPDATE] suggestion in dormdetail (maybe change to stateless)

--- lib/screens/dorm_detail.dart
+++ lib/screens/dorm_detail.dart
@@ -6,6 +6,10 @@ import 'package:moddormy_flutter/widgets/review.dart';
 
 class DormDetail extends StatefulWidget {
   const DormDetail({super.key});
+  // final String id = ModalRoute.of(context).settings.arguments;
+  // In this example, we're passing a dorm id as a dynamic parameter in the route ('/dorms/:id'),
+  // and extracting it in the DormDetailsPage using ModalRoute.of(context).settings.arguments.
+  // You can replace dorm_123 with an actual dorm id variable, or fetch it from an API or database.
 
   @override
   State<DormDetail> createState() => _DormDetailState();
@@ -14,6 +18,7 @@ class DormDetail extends StatefulWidget {
 class _DormDetailState extends State<DormDetail> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: const Color(0xffFFF8F0),
       appBar: const MyAppbar(),
