3821f83a4c6050a8e2bdb339297e7ceaf784a208
Lilyliiin <98150547+Lilyliiin@users.noreply.github.com>
2023-05-11 04:39:07 +0700

    update profile แบบไม่บัค

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -51,215 +51,199 @@ class _ProfilePageState extends State<ProfilePage> {
           body: Padding(
               padding: const EdgeInsets.all(25.0),
               child: Column(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(children: [
-                      Container(
-                        // width: 150,
-                        // height: 150,
-                        // decoration: BoxDecoration(
-                        //   shape: BoxShape.circle),
-                        child: Stack(children: [
-                          user.profileImage == "" || user.profileImage == null
-                              ? const CircleAvatar(
-                                  backgroundImage: AssetImage(
-                                    'assets/images/profileNull.png',
-                                  ),
-                                  radius: 80,
-                                )
-                              : CircleAvatar(
-                                  radius: 80,
-                                  backgroundImage: NetworkImage(
-                                    "http://moddormy.ivelse.com:8000${user.profileImage}",
-                                  ),
-                                ),
-                        ]),
-                      ),
+                Column(children: [
+                  Container(
+                    // width: 150,
+                    // height: 150,
+                    // decoration: BoxDecoration(
+                    //   shape: BoxShape.circle),
+                    child: Stack(children: [
+                      user.profileImage == "" || user.profileImage == null
+                          ? const CircleAvatar(
+                              backgroundImage: AssetImage(
+                                'assets/images/profileNull.png',
+                              ),
+                              radius: 80,
+                            )
+                          : CircleAvatar(
+                              radius: 80,
+                              backgroundImage: NetworkImage(
+                                "http://moddormy.ivelse.com:8000${user.profileImage}",
+                              ),
+                            ),
                     ]),
-                    const SizedBox(height: 10),
-                    Text(
-                      user.username,
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                    const Padding(padding: EdgeInsets.only(bottom: 20)),
-                  ],
+                  ),
+                ]),
+                const SizedBox(height: 10),
+                Text(
+                  user.username,
+                  style: const TextStyle(fontSize: 24),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
+                const Padding(padding: EdgeInsets.only(bottom: 20)),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Row(
                         children: [
-                          Row(
-                            children: [
-                              const Text(
-                                "Profile",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 18),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          const EditUserForm(),
-                                    ),
-                                  );
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.only(left: 8.0),
-                                  child: Image.asset(
-                                    'assets/images/edit_black.png',
-                                    width: 19,
-                                    height: 19,
-                                  ),
-                                ),
-                              ),
-                            ],
+                          const Text(
+                            "Profile",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(22, 8, 8, 8),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    //Name
-                                    if (user.firstname != null &&
-                                        user.lastname != null &&
-                                        user.firstname != "" &&
-                                        user.lastname != "")
-                                      Expanded(
-                                        child: Text(
-                                          name,
-                                          // style: const TextStyle(
-                                          //   fontSize: 15,
-                                          // ),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      )
-                                    else
-                                      const Text("Name  :     -"),
-                                    const Padding(
-                                        padding: EdgeInsets.only(bottom: 4)),
-                                    //Tel
-                                    if (user.tel != null && user.tel != "")
-                                      Expanded(
-                                          child: Text(
-                                        "Tel       :   ${user.tel}",
-                                        overflow: TextOverflow.ellipsis,
-                                      ))
-                                    else
-                                      const Text("Tel        :     -"),
-                                    const Padding(
-                                        padding: EdgeInsets.only(bottom: 4)),
-                                    //Email
-                                    if (user.email != null && user.email != "")
-                                      Expanded(
-                                        child: Text(
-                                          "Email  :    ${user.email}",
-                                          //style: const TextStyle(fontSize: 15),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      )
-                                    else
-                                      const Text("Email  :     -"),
-                                    const Padding(
-                                        padding: EdgeInsets.only(bottom: 4)),
-                                    //LineID
-                                    if (user.lineId != null &&
-                                        user.lineId != "")
-                                      Expanded(
-                                        child: Text(
-                                          "LineID :    ${user.lineId}",
-                                          //style: const TextStyle(fontSize: 15),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      )
-                                    else
-                                      const Text("LineID  :     -"),
-                                  ],
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const EditUserForm(),
                                 ),
-                              ],
+                              );
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Image.asset(
+                                'assets/images/edit_black.png',
+                                width: 19,
+                                height: 19,
+                              ),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(22, 8, 8, 8),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          //mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            if (user.firstname != null &&
+                                user.lastname != null &&
+                                user.firstname != "" &&
+                                user.lastname != "")
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      name,
+                                      // style: const TextStyle(
+                                      //   fontSize: 15,
+                                      // ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  )
+                                ],
+                              )
+                            else
+                              const Text("Name  :     -"),
+                            const Padding(padding: EdgeInsets.only(bottom: 4)),
+                            if (user.tel != null && user.tel != "")
+                              Row(
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                    "Tel       :   ${user.tel}",
+                                    overflow: TextOverflow.ellipsis,
+                                  ))
+                                ],
+                              )
+                            else
+                              const Text("Tel        :     -"),
+                            const Padding(padding: EdgeInsets.only(bottom: 4)),
+                            if (user.email != null && user.email != "")
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      "Email  :    ${user.email}",
+                                      //style: const TextStyle(fontSize: 15),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  )
+                                ],
+                              )
+                            else
+                              const Text("Email  :     -"),
+                            const Padding(padding: EdgeInsets.only(bottom: 4)),
+                            if (user.lineId != null && user.lineId != "")
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      "LineID :    ${user.lineId}",
+                                      //style: const TextStyle(fontSize: 15),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  )
+                                ],
+                              )
+                            else
+                              const Text("LineID  :     -"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 if (user.userType == "DormOwner")
-                  Row(children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(10, 13, 0, 10),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text(
-                                    "Dorm Description",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                    ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 13, 0, 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "Dorm Description",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
                                   ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(width: 70),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const PostForm()),
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color.fromARGB(
-                                    237, 215, 106, 56), // set background color
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 1, vertical: 1),
-                                minimumSize: const Size(100, 30),
-                              ),
-                              child: const Text(
-                                "Add Dorm",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 255, 255, 255),
-                                  fontWeight: FontWeight.bold,
                                 ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 70),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const PostForm()),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color.fromARGB(
+                                  237, 215, 106, 56), // set background color
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 1, vertical: 1),
+                              minimumSize: const Size(100, 30),
+                            ),
+                            child: const Text(
+                              "Add Dorm",
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ],
-                        ),
-                        Row(
-                          children: const [
-                            SizedBox(
-                              width: 335,
-                              height: 250,
-                              child: Center(
-                                child: DormsEachOwner(),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ]),
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: SizedBox(
+                            // constraints: BoxConstraints(
+                            //   minWidth: 1,
+                            //   maxWidth: 350.0,
+
+                            // ),
+                            width: width * 0.8,
+                            height: height * 0.3,
+                            child: const DormsEachOwner()),
+                      ),
+                    ],
+                  ),
               ])));
     } else {
       return Scaffold(
